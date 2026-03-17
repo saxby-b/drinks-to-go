20fd06cbb3bb28ceff777d3585e4e41c21f0797a
C. Saxby Bain <csaxbybain@yahoo.com>
2026-03-17 14:35:48 -0400

    Added to checkAge function

--- js/script.js
+++ js/script.js
@@ -2,16 +2,23 @@ const input = document.querySelector("input");
 const button =document.querySelector("button");
 const question = document.querySelector(".question");
 const message = document.querySelector(".message");
+const softDrinks = document.querySelector(".soft-drinks");
+const alcoholicDrinks = document.querySelector(".alcoholic-drinks");
 
 const checkAge = function() {
     const age = input.value;
     const legalAge = 21;
     if(age < legalAge) {
         message.innerText = "Drinks To Go offers many soft drinks such as sodas.";
-
+        softDrinks.classList.remove("hide");
     } else {
         message.innerText = "Drinks To Go has a large selection of acholic and non-alcoholic beverages."
+        softDrinks.classList.remove("hide");
+        alcoholicDrinks.classList.remove("hide");
     }
+    input.classList.add("hide");
+    button.classList.add("hide");
+    question.classList.add("hide");
 }
 
 button.addEventListener("click", checkAge);
